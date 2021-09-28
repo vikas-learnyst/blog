bc04efd54de6cfc7fcd31591c137711d3f0991d2
vikas reddy <vikas@learnyst.com>
2021-09-28 21:33:22 +0530

    second commit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,17 +1,7 @@
 class ArticlesController < ApplicationController
   
   def index
-    #@user=User.find(params[:user_id])
-   # @articles = Article.all
     render json: @current_user.articles
-    #render json:{articles: @current_user.articles.as_json(only:[:title,:body])}
-    #@articles.each do|a|
-     # render json: {article_comments: a.comments}
-    #end
-    
-
-    
-
   end
   def show
    # @user=User.find(params[:user_id])
